57d67d75137100d9072abf1097c64434d02228af
Eugene Petrenko <me@jonnyzzz.com>
2019-08-23 13:11:15 +0200

    use real 1.3.50

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,1 @@
-pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-dev")
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
-
-    resolutionStrategy {
-      eachPlugin {
-        if (requested.version == "1.3.50") {
-          useVersion("1.3.50-release-105")
-        }
-      }
-    }
-  }
-}
-
 
